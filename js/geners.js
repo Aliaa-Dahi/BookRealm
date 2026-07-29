1e2a1d1042b679a07b767559e40e3051e720ab72
Aliaa <aliaamohamaddahi@gmail.com>
2026-07-30 00:10:13 +0300

    feat: implement dynamic book genre selection and rendering with OpenLibrary API integration

--- js/geners.js
+++ js/geners.js
@@ -26,4 +26,21 @@ export const generes = [
 const generesContainer = document.querySelector(".generes-container");
 if (generesContainer) {
     generesContainer.innerHTML = GeneresContainer(generes);
+    
+    generesContainer.addEventListener('click', (e) => {
+        // .closest() finds the .card no matter if they clicked the span or the card-body!
+        const card = e.target.closest('.card');
+        
+        if(card){
+            // .trim() removes the extra empty spaces/newlines around the text
+            // console.log(card.textContent.trim());
+            getByGenere(card.textContent.trim())
+        }
+    });
+}
+
+async function getByGenere(genere){
+    const response = await fetch(`https://openlibrary.org/subjects/${genere}.json`);
+    const data = await response.json()
+    console.log(data)
 }
