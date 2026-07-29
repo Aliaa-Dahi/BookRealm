a6eb4e388799ae96a00c706863d1e87409fc7992
Aliaa <aliaamohamaddahi@gmail.com>
2026-07-29 18:32:24 +0300

    feat: implement mobile responsive navigation menu with offcanvas and custom styling

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import 'bootstrap';
 
+// Custom Styles
+import './css/common.css';
+import './css/nav.css';
+
 // From Files
 import { updateActiveLink } from './js/nav.js';
 
