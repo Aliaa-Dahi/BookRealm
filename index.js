fe88a2ee25621389153c9fa0c896530c3c85d708
Aliaa <aliaamohamaddahi@gmail.com>
2026-08-06 16:37:06 +0300

    build: generate production assets and update project files

--- index.js
+++ index.js
@@ -1,26 +1,61 @@
 // Libraries
-import 'bootstrap/dist/css/bootstrap.min.css'; 
-import 'bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 // Custom Styles
-import './css/common.css';
-import './css/home.css'
+import "./css/common.css";
+import "./css/home.css";
 
 // Components
-import Nav, { updateActiveLink } from './componenets/Nav/nav.js';
-import { GeneresContainer } from './componenets/GeneresContainer/generes-container.js';
+import Nav, { updateActiveLink } from "./componenets/Nav/nav.js";
+import { GeneresContainer } from "./componenets/GeneresContainer/generes-container.js";
 
 const navContainer = document.querySelector(".nav-container");
 if (navContainer) {
-    navContainer.innerHTML = Nav();
+  navContainer.innerHTML = Nav();
 }
+
 
 const homeGeneresContainer = document.querySelector(".home-generes-container");
 if (homeGeneresContainer) {
-    homeGeneresContainer.innerHTML = GeneresContainer(["fantasy", "romance", "thriller", "mystery", "science fiction", "fantasy", "romance", "thriller", "mystery", "science fiction"]);
+  homeGeneresContainer.innerHTML = GeneresContainer([
+    "fantasy",
+    "romance",
+    "thriller",
+    "mystery",
+    "science fiction",
+    "fantasy",
+    "romance",
+    "thriller",
+    "mystery",
+    "science fiction",
+  ]);
 }
-
 
 // Initialize the active state immediately after injecting the Nav HTML
 updateActiveLink();
+
+// function route(r) {
+//   // console.log(window.location.pathname)
+//   window.history.pushState({}, "", r)
+//   showContent()
+//   // window.location.pathname = r;
+// }
+
+// window.addEventListener('popstate', ()=>{
+//   showContent()
+// })
+
+// let card = document.getElementById("card");
+// // showContent()
+// card.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   route("pages");
+// });
+
+// function showContent() {
+//   console.log(window.location.pathname);
+//   // Change "pages" to "/pages"
+//   if (window.location.pathname == "/pages") card.classList.add("d-none");
+// }
 
